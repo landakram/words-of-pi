184c40264d379a1b0822f86668ac3994d153083c
Mark Hudnall <hudnall.mark@gmail.com>
2012-12-01 21:05:22 -0800

    Initial commit.

---
+++ pi.py
@@ -0,0 +1,26 @@
+import math
+from functools import partial
+import operator as op
+
+chars = 'abcdefghijklmnopqrstuvxyz.? '
+def encoding(number):
+    return chars[number % len(chars)]
+
+if __name__ == '__main__':
+    st = ''
+    with open('pi.txt') as f:
+        pi = f.read()
+        for a, b in zip(*(iter(pi),) * 2):
+            d = int(a + b)
+            st += encoding(d)
+
+    words = set()
+    with open('/usr/share/dict/words') as d:
+        for line in d:
+            words.add(line.strip())
+
+    for s in st.split(' '):
+        if s in words and len(s) > 3:
+            print s
+
+
